ef289bd26432ad671b97d64595393fa3cb0f1d2e
Israel Nieto <isra.utch@gmail.com>
2019-09-26 11:05:57 -0600

    Lesson 17 __call and __callStatic

--- public/index.php
+++ public/index.php
@@ -1,21 +1,36 @@
 <?php
 
+use Styde\HtmlNode;
 use Styde\User;
 
 require "../vendor/autoload.php";
 
-$user =  new User([
-    'first_name' => 'Israel',
-    'last_name' => 'Nieto'
-]);
+//$user =  new User([
+//    'first_name' => 'Israel',
+//    'last_name' => 'Nieto'
+//]);
+//
+//$user->nickname = 'inieto';
+//
+//
+//echo "<p>Hola {$user->first_name} {$user->last_name}</p>";
+//
+//if ($user->hasAttribute('nickname')){
+//    echo "<p>Nickname: {$user->nickname}";
+//}
 
-$user->nickname = 'inieto';
+$node = (new HtmlNode('textarea','Hola tonto'))
+        ->name('first_name')
+        ->id('first_name')
+        ->type('textarea')
+        ->placeholder('First Name');
+
+echo $node->render();
+
+$static =  HtmlNode::h1('Titulo de la clase')
+    ->id('title')
+    ->style('color:blue;');
 
 
-echo "<p>Hola {$user->first_name} {$user->last_name}</p>";
-
-if ($user->hasAttribute('nickname')){
-    echo "<p>Nickname: {$user->nickname}";
-}
-
+echo $static->render();
 
